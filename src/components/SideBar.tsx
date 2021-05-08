300ff9fdc3ad5133dbcdd11f4fbf072b7f34330b
wellington <wlazaro52@gmail.com>
2021-05-08 20:43:13 -0300

    completo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,48 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from '../components/Button';
+
+export function SideBar({state}) {
+
+  interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }
+
+
+  function handleClickButton(id: number) {
+    state.setSelectedGenreId(id);
+  }
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+    
+    
+  }, []);
+  
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={state.selectedGenreId === genre.id}
+              />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
